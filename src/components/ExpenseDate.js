39efde275287bb944f71cd5dc7b583fe6856f7a4
allanlalangan <73400708+allanlalangan@users.noreply.github.com>
2022-01-27 19:35:27 -0800

    added styles to ExpenseDate component

--- src/components/ExpenseDate.js
+++ src/components/ExpenseDate.js
@@ -1,13 +1,15 @@
+import "./ExpenseDate_CourseTemplate.css";
+
 function ExpenseDate(props) {
   const month = props.date.toLocaleString("en-US", { month: "long" });
   const day = props.date.toLocaleString("en-US", { day: "2-digit" });
   const year = props.date.getFullYear();
 
   return (
-    <div>
-      <p>{month}</p>
-      <p>{day}</p>
-      <p>{year}</p>
+    <div className="expense-date">
+      <div className="expense-date__month">{month}</div>
+      <div className="expense-date__day">{day}</div>
+      <div className="expense-date__year">{year}</div>
     </div>
   );
 }
