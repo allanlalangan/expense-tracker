eabf8c3b7637d2f85fcc27d6542f7f96abd63ac9
allanlalangan <73400708+allanlalangan@users.noreply.github.com>
2022-01-29 14:47:09 -0800

    created Card component with children props. Card replaces Expenses and ExpenseItem wrapper divs for better composition.
    Card component eliminates repitition of certain style code.

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,17 +1,18 @@
 import "./ExpenseItem_CourseTemplate.css";
+import Card from "./Card";
 import ExpenseDate from "./ExpenseDate";
 
 function ExpenseItem(props) {
   const { id, title, amount, date } = props;
 
   return (
-    <article key={id} className="expense-item">
+    <Card key={id} className="expense-item">
       {<ExpenseDate date={date} />}
       <div className="expense-item__description">
         <h2>{title}</h2>
         <p className="expense-item__price">${amount}</p>
       </div>
-    </article>
+    </Card>
   );
 }
 
--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,13 +1,14 @@
 import ExpenseItem from "./ExpenseItem";
+import Card from "./Card";
 import "./Expenses_CourseTemplate.css";
 
 function Expenses(props) {
   return (
-    <div className="expenses">
+    <Card className="expenses">
       {props.data.map((expense) => {
         return <ExpenseItem {...expense} />;
       })}
-    </div>
+    </Card>
   );
 }
 
