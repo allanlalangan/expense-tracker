5fca2b082059161fa919155084f267a0cfc7f09e
allanlalangan <73400708+allanlalangan@users.noreply.github.com>
2022-01-31 22:19:06 -0800

    import ExpensesFilter into Expenses component. passed selected option value as filteredYear state.

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,10 +1,20 @@
+import { useState } from "react";
+import ExpensesFilter from "./ExpensesFilter";
 import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 import "./Expenses_CourseTemplate.css";
 
 const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState();
+  const selectYearHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
   return (
     <Card className="expenses">
+      <ExpensesFilter
+        selectedOption={filteredYear}
+        onSelectYear={selectYearHandler}
+      />
       {props.data.map((expense) => {
         return <ExpenseItem {...expense} key={expense.id} />;
       })}
--- src/components/Expenses/ExpensesFilter.js
+++ src/components/Expenses/ExpensesFilter.js
@@ -1,13 +1,15 @@
-import React from "react";
-
 import "./ExpensesFilter.css";
 
-const ExpensesFilter = () => {
+const ExpensesFilter = (props) => {
+  const selectYearHandler = (e) => {
+    props.onSelectYear(e.target.value);
+  };
+
   return (
     <div className="expenses-filter">
       <div className="expenses-filter__control">
         <label>Filter by year</label>
-        <select>
+        <select value={props.selectedOption} onChange={selectYearHandler}>
           <option value="2022">2022</option>
           <option value="2021">2021</option>
           <option value="2020">2020</option>
