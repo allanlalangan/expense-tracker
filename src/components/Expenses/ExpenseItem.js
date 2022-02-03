058d609d437d3e941b3e7fd2e4bf9433165275e3
allanlalangan <73400708+allanlalangan@users.noreply.github.com>
2022-02-02 21:32:04 -0800

    improved semantics by wrapping ExpenseItem JSX inside li element.

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -3,16 +3,18 @@ import Card from "../UI/Card";
 import ExpenseDate from "./ExpenseDate";
 
 const ExpenseItem = (props) => {
-  const { id, title, amount, date } = props;
+  const { title, amount, date } = props;
 
   return (
-    <Card className="expense-item">
-      {<ExpenseDate date={date} />}
-      <div className="expense-item__description">
-        <h2>{title}</h2>
-        <p className="expense-item__price">${amount}</p>
-      </div>
-    </Card>
+    <li>
+      <Card className="expense-item">
+        {<ExpenseDate date={date} />}
+        <div className="expense-item__description">
+          <h2>{title}</h2>
+          <p className="expense-item__price">${amount}</p>
+        </div>
+      </Card>
+    </li>
   );
 };
 
