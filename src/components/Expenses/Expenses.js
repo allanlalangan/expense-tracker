f3f3433ac50db3abaddba437305f53951a47e8da
allanlalangan <73400708+allanlalangan@users.noreply.github.com>
2022-02-02 21:24:23 -0800

    created ExpensesList component and nested inside Expenses component
    ExpenseItems and fallback message are rendered inside ExpensesList component.

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
+import ExpensesList from "./ExpensesList";
 import ExpensesFilter from "./ExpensesFilter";
-import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 import "./Expenses_CourseTemplate.css";
 
@@ -10,41 +10,24 @@ const Expenses = (props) => {
     setFilteredYear(selectedYear);
   };
 
-  const filterExpensesByYear = props.data.filter(
+  const allExpenses = props.data;
+  const filteredExpensesByYear = props.data.filter(
     (expense) => expense.date.getFullYear().toString() === filteredYear
   );
 
-  let expensesContent = <p>No Expenses For {filteredYear}</p>;
-
-  if (filterExpensesByYear.length > 0) {
-    expensesContent = filterExpensesByYear.map((expense) => (
-      <ExpenseItem {...expense} key={expense.id} />
-    ));
-  }
-
-  if (filteredYear === undefined || filteredYear === "Show All") {
-    return (
-      <Card className="expenses">
-        <ExpensesFilter
-          selectedOption={filteredYear}
-          onSelectYear={selectYearHandler}
-        />
-        {props.data.map((expense) => {
-          return <ExpenseItem {...expense} key={expense.id} />;
-        })}
-      </Card>
-    );
-  } else {
-    return (
-      <Card className="expenses">
-        <ExpensesFilter
-          selectedOption={filteredYear}
-          onSelectYear={selectYearHandler}
-        />
-        {expensesContent}
-      </Card>
-    );
-  }
+  return (
+    <Card className="expenses">
+      <ExpensesFilter
+        selectedOption={filteredYear}
+        onSelectYear={selectYearHandler}
+      />
+      {filteredYear === "Show All" || filteredYear === undefined ? (
+        <ExpensesList items={allExpenses} />
+      ) : (
+        <ExpensesList items={filteredExpensesByYear} />
+      )}
+    </Card>
+  );
 };
 
 export default Expenses;
---
+++ src/components/Expenses/ExpensesList.js
@@ -0,0 +1,18 @@
+import ExpenseItem from "./ExpenseItem";
+import "./ExpensesList.css";
+
+const ExpensesList = (props) => {
+  if (props.items.length === 0) {
+    return <h2 className="expenses-list__fallback">No Expenses Found</h2>;
+  }
+
+  return (
+    <ul className="expenses-list">
+      {props.items.map((expense) => {
+        return <ExpenseItem {...expense} key={expense.id} />;
+      })}
+    </ul>
+  );
+};
+
+export default ExpensesList;
