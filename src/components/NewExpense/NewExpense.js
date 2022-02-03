7a3d68a0adb062f91c2083abc84985d7bc63c3c9
allanlalangan <73400708+allanlalangan@users.noreply.github.com>
2022-02-02 22:35:13 -0800

    ExpenseForm is initially hidden. Button opens form, which can be closed with cancel button.
    Adding new expense also closes form after submitting.

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,20 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 
 import ExpenseForm from "./ExpenseForm";
 import "./NewExpense.css";
 
 const NewExpense = (props) => {
+  const [isFormOpen, setIsFormOpen] = useState(false);
+  const openOrClose = () => {
+    !isFormOpen ? setIsFormOpen(true) : setIsFormOpen(false);
+  };
+
   const saveNewExpense = (newExpenseData) => {
     const newExpense = {
       ...newExpenseData,
     };
-
     props.onAddExpense(newExpense);
+    openOrClose();
   };
 
   return (
     <div className="new-expense">
-      <ExpenseForm onSubmitExpense={saveNewExpense} />
+      {!isFormOpen ? (
+        <button onClick={openOrClose}>Click to Add New Expense</button>
+      ) : (
+        <ExpenseForm onCancel={openOrClose} onSubmitExpense={saveNewExpense} />
+      )}
     </div>
   );
 };
